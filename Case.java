3c3f7e763027120eebfdbe04e76b29bd94b33c42
chaimae29 <95635004+chaimae29@users.noreply.github.com>
2021-12-13 14:30:46 +0100

    Update Case.java

--- Case.java
+++ Case.java
@@ -25,12 +25,17 @@ public class Case{
   * pré-requis : let est une lettre majuscule
   */
   public void setLettre (char let){
-    
+    this.Lettre=let;
+  }
 
   /**
   * résultat : vrai ssi la case est recouverte par une lettre
   */
   public boolean estRecouverte (){
-
+    boolean recouverte=false;
+    if(this.Lettre!=""){recouverte=true;}
+    return recouverte;
+  }
   public String toString (){
+   return ("uneCouleur : "+ this.uneCouleur + "Lettre : "+ this.Lettre);
  }
