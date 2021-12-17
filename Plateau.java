14416241182148befd083b802d0f06e3226738a4
chaimae29 <95635004+chaimae29@users.noreply.github.com>
2021-12-17 14:46:33 +0100

    Update Plateau.java

--- Plateau.java
+++ Plateau.java
@@ -1,35 +1,61 @@
 public class Plateau{
-private Case [][] g; // g pour grille
+  private Case [][] g; // g pour grille
+
+  public Plateau(){
+    int [][] plateau = {
+      {5,1,1,2,1,1,1,5,1,1,1,2,1,1,5},
+      {1,4,1,1,1,3,1,1,1,3,1,1,1,4,1},
+      {1,1,4,1,1,1,2,1,2,1,1,1,4,1,1},
+      {2,1,1,4,1,1,1,2,1,1,1,4,1,1,2},
+      {1,1,1,1,4,1,1,1,1,1,4,1,1,1,1},
+      {1,3,1,1,1,3,1,1,1,3,1,1,1,3,1},
+      {1,1,2,1,1,1,2,1,2,1,1,1,2,1,1},
+      {5,1,1,2,1,1,1,4,1,1,1,2,1,1,5},
+      {1,1,2,1,1,1,2,1,2,1,1,1,2,1,1},
+      {1,3,1,1,1,3,1,1,1,3,1,1,1,3,1},
+      {1,1,1,1,4,1,1,1,1,1,4,1,1,1,1},
+      {2,1,1,4,1,1,1,2,1,1,1,4,1,1,2},
+      {1,1,4,1,1,1,2,1,2,1,1,1,4,1,1},
+      {1,4,1,1,1,3,1,1,1,3,1,1,1,4,1},
+      {5,1,1,2,1,1,1,5,1,1,1,2,1,1,5}};
   
-  int [][] plateau = {
-    {5,1,1,2,1,1,1,5,1,1,1,2,1,1,5},
-    {1,4,1,1,1,3,1,1,1,3,1,1,1,4,1},
-    {1,1,4,1,1,1,2,1,2,1,1,1,4,1,1},
-    {2,1,1,4,1,1,1,2,1,1,1,4,1,1,2},
-    {1,1,1,1,4,1,1,1,1,1,4,1,1,1,1},
-    {1,3,1,1,1,3,1,1,1,3,1,1,1,3,1},
-    {1,1,2,1,1,1,2,1,2,1,1,1,2,1,1},
-    {5,1,1,2,1,1,1,4,1,1,1,2,1,1,5},
-    {1,1,2,1,1,1,2,1,2,1,1,1,2,1,1},
-    {1,3,1,1,1,3,1,1,1,3,1,1,1,3,1},
-    {1,1,1,1,4,1,1,1,1,1,4,1,1,1,1},
-    {2,1,1,4,1,1,1,2,1,1,1,4,1,1,2},
-    {1,1,4,1,1,1,2,1,2,1,1,1,4,1,1},
-    {1,4,1,1,1,3,1,1,1,3,1,1,1,4,1},
-    {5,1,1,2,1,1,1,5,1,1,1,2,1,1,5}};
+    this.g=new Case[15][15];
+
+
+    for (int i=0; i!=plateau.length;i++){
+      for (int j=0; j!=plateau[i].length;j++){
+        g[i][j]= new Case(plateau[i][j]);
+      }
+    }
+  }
   
+
+
   /**
   * résultat : chaîne décrivant ce Plateau
   */
   public String toString(){
-  int nbLigne=0;
-  int nbColonne=0;
-    for(int i=0;i<nbLigne;i++){
-      for(int j=0;j<nbColonne;j++){
-       System.out.print(" | "+ g[i][j]);
+  String affichage="";
+    for(int i=0;i<this.g.length;i++){
+      affichage += "\n";
+      for(int k=0;k<this.g.length+1;k++){affichage +="----";}
+      affichage+="\n | ";
+      for(int j=0;j<this.g[i].length;j++){
+        char Lettre = g[i][j].getLettre();
+        if(Lettre==' '){
+          int couleur = g[i][j].getCouleur();
+          affichage += couleur +" | " ;
+        }
+        else{
+          affichage += Lettre +" | " ;
+        }
       }
     }
+  return affichage;
+  }
 
-    
+  public static void main(String args[]){ 
+    Plateau p=new Plateau();
+    System.out.println(p);
   }
 }
