3ed5a99c2c8252e3b3e9763b56269a7042e9421d
chaimae29 <95635004+chaimae29@users.noreply.github.com>
2022-01-04 11:54:27 +0100

    Update Plateau.java

--- Plateau.java
+++ Plateau.java
@@ -59,7 +59,6 @@ public class Plateau{
     Plateau p=new Plateau();
     System.out.println(p);
   }
-
   
 /**
 * pré-requis : mot est un mot accepté par CapeloDico,
@@ -74,18 +73,47 @@ public boolean placementValide(String mot, int numLig, int numCol,char sens, MEE
   boolean placement = false;
   int k=0;
   int j=numCol;
+  int i=numLig;
   int tab[] = e.tabFreq;
+  boolean lettre_dispo = true;
+  boolean case_vide_autour = true;
   if(e.estVide()){
     if(mot.length() <= 2){return placement;}
-    while(k<mot.length() || e.tabFreq[k] !=0){ 
-      
+    while(k<mot.length() && e.tabFreq[k] !=0 && lettre_dispo){ 
+      if (e.tabFreq[k] == 0){
+        lettre_dispo = false;
+        placement = false;}
+      else if (k =mot.length() && lettre_dispo){placement = true;}
+      k += 1;
     }
     if(sens == 'h' && numLig == 8){
       while(j<mot.length() || placement == false){
-        j += 1;
         if(j == 8){placement = true;}
+        j += 1;
+      }
+    }
+    else if(sens == 'v' && numCol == 8){
+      while(i<mot.length() || placement == false){
+        if(i == 8){placement = true;}
+        i += 1;
+      }
+    }
+  }
+  else{
+    if(sens == 'h'){
+      while (k<mot.length() &&  j<=15 && case_vide_autour){
+        if (j>15){placement = false;}
+        k += 1;
+        j += 1;
+      }
+    }
+    else if(sens == 'v'){
+      while (k<mot.length() &&  i<=15 && case_vide_autour){
+        if (i>15){placement = false;}
+        k += 1;
+        i += 1;
       }
     }
   }
 }
-
+}
